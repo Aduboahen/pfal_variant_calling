95f716d1c30eaa6363e5813f1c1b1b700ac1999b
Aduboahen <oseimensa@outlook.com>
2022-12-06 14:43:11 +0000

    minor edit

--- scripts/test_run.sh
+++ scripts/test_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker run -i -t --rm \ 
+docker run -i -t --rm \
+	--mount type=bind,source=${PWD}/output,target=/tmp \
 	mugicrow/pfal_variant_call:latest \
        	nextflow run \
 	pf.resistance.nf \
 	--sampleid test \
 	--outDir ./output \
-	--reads ./test_fq/test_{1,2}.fastq.gz \
+	--reads "./test_fq/test_{1,2}.fastq.gz" \
 	-resume
