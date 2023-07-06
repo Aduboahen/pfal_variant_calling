941171bc6d27b196e889e7d858115f94dd463b48
Aduboahen <oseimensa@outlook.com>
2023-07-06 19:19:04 +0000

    minor edit

--- scripts/create_run_script.py
+++ scripts/create_run_script.py
@@ -8,14 +8,13 @@ import os
 
 from datetime import date
 
-today=date.today().strftime('%Y_%m_%d')
+today = date.today().strftime('%Y_%m_%d')
 pwd = os.getcwd()
 
-with open("runscript.sh", 'w') as outfile:
-
+with open("runscript.sh", 'w', encoding="utf8") as outfile:
 	for filename in os.listdir(os.path.join(pwd, "input")):
-		if filename.endswith("_1.fastq"):
-			sample=filename.split('_')
-			sampleid=sample[0]
-			sample_code=sample[1]
-			outfile.write(f'nextflow run {os.path.join(pwd, "pf.resistance.nf")} --sampleid {sampleid} --outDir {os.path.join(pwd, "output", today)} --reads "{os.path.join(pwd, "input", sampleid + "_{{1,2}}.fastq.gz")}" -resume --threads 50 -with-report -with-timeline\n')
+		if filename.endswith("_1.fastq.gz"):
+			sample = filename.split('_')
+			sampleid = sample[0]
+			sample_code = sample[1]
+			outfile.write(f'nextflow run {os.path.join(pwd, "pf.resistance.nf")} --sampleid {sampleid} --outDir {os.path.join(pwd, "output", today)} --reads "{os.path.join(pwd, "input", sampleid + "_{1,2}.fastq.gz")}" -resume --threads 50 -with-report -with-timeline\n')
