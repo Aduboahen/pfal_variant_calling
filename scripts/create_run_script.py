98d0b3dd0d341462f5439edb396d83b99a68e378
James Osei-Mensa <janusisgrt@gmail.com>
2023-07-06 18:27:44 +0000

    added python script to generate nextflow run commands

--- scripts/create_run_script.py
+++ scripts/create_run_script.py
@@ -9,12 +9,13 @@ import os
 from datetime import date
 
 today=date.today().strftime('%Y_%m_%d')
+pwd = os.getcwd()
 
-with open("run_script.sh", 'w') as outfile:
+with open("runscript.sh", 'w') as outfile:
 
-	for filename in os.listdir(os.getcwd()):
+	for filename in os.listdir(os.path.join(pwd, "input")):
 		if filename.endswith("_1.fastq"):
 			sample=filename.split('_')
 			sampleid=sample[0]
 			sample_code=sample[1]
-			outfile.write(f'nextflow run fastp.nf --sampleid {sampleid} -resume --outDir ./output --reads "{sampleid}_{{1,2}}.fastq" -resume\n')
+			outfile.write(f'nextflow run {os.path.join(pwd, "pf.resistance.nf")} --sampleid {sampleid} --outDir {os.path.join(pwd, "output", today)} --reads "{os.path.join(pwd, "input", sampleid + "_{{1,2}}.fastq.gz")}" -resume --threads 50 -with-report -with-timeline\n')
