0c6375cc69b67502b89a594c4dd6e8cc0d2c890a
Aduboahen <oseimensa@outlook.com>
2025-05-23 11:45:14 +0200

    indentation

--- scripts/test_run.sh
+++ scripts/test_run.sh
@@ -3,7 +3,7 @@
 docker run -i -t --rm \
 	--mount type=bind,source=${PWD}/output,target=/tmp \
 	mugicrow/pfal_variant_call:latest \
-       	nextflow run \
+  nextflow run \
 	pf.resistance.nf \
 	--sampleid test \
 	--outDir ./output \
