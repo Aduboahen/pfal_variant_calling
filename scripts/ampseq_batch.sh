0736e4a38c2d6cbec1662af5570aec84a9caa305
Aduboahen <oseimensa@outlook.com>
2025-05-23 11:44:05 +0200

    ampseq batch run

---
+++ scripts/ampseq_batch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+today=$(date +"%d-%B-%y")
+workflow="${1:-./ampseq.nf}"
+input="${2:-./input/}"
+suffix1="${3:-_R1}"
+suffix2="${4:-.fastq.gz}"
+thread="${5:-6}"
+
+for file in ./input/*${suffix1}${suffix2}*; do
+	sampleid=$(basename $file ${suffix1}${suffix2})
+
+	nextflow run ${workflow} --sampleid ${sampleid} --reads ./input/"${sampleid}_{R1,R2}.fastq.gz" --outDir ./output/${today} --threads ${thread} --bedfile ./reference/amplicon_len.bed --reference ./reference/fastaseq_AmpSeq_ASAAP_Fragments_v2.fasta -resume
+done
